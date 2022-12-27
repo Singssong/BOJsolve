84532544c43231ad71c9c33ff6afc2944219b460
HJRYU\o__hy <o__hyeah@naver.com>
2022-12-27 11:09:54 +0900

    15649

--- 15649_N과 M(1).py
+++ 15649_N과 M(1).py
@@ -18,7 +18,7 @@ def recursive(num):
   if(num == M):
     print(" ".join(map(str, result)))
     return
-  for i in range(1,N+1):
+  for i in range(num+1,N+1):
     if(visited[i] == False):
       visited[i] = True
       result.append(i)
